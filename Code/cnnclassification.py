067ed2d9c7eae3767ca6c571aa9fea242619b4ae
Mehmet Atakan Kalkar <43787387+atakankalkar@users.noreply.github.com>
2020-05-25 21:52:46 +0300

    Update cnnclassification.py

--- Code/cnnclassification.py
+++ Code/cnnclassification.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar 26 15:18:53 2020
-
 @author: ataka
 """
 
@@ -26,13 +25,15 @@ plt.imshow(X[31])
 plt.axis('off')
 """
 
+testsize = 0.2      ##test size parameter
+
 #Splitting data to train and test
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.15, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=testsize, random_state=42)
 number_of_train = X_train.shape[0]
 number_of_test = X_test.shape[0]
-X_train=X_train.reshape(279,128,128,1) 
-X_test =X_test.reshape(50,128,128,1)
+X_train=X_train.reshape(number_of_train,128,128,1) 
+X_test =X_test.reshape(number_of_test,128,128,1)
 X_val = X_test
 Y_val = Y_test
 
@@ -54,6 +55,7 @@ from keras.callbacks import ReduceLROnPlateau
 
 model = Sequential()
 #
+##Convolution Layers (Feature Extraction)
 model.add(Conv2D(filters = 8, kernel_size = (3,3),padding = 'Same', 
                  activation ='relu', input_shape = (128,128,1)))
 model.add(MaxPool2D(pool_size=(2,2)))
@@ -70,7 +72,7 @@ model.add(Conv2D(filters = 8, kernel_size = (3,3),padding = 'Same',
                  activation ='tanh'))
 model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
 
-# fully connected
+# fully connected Artificial Neural Network
 model.add(Flatten())
 model.add(Dense(128, activation = "relu"))
 model.add(Dense(128, activation = "relu"))
@@ -83,9 +85,8 @@ optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999) #Adam optimizer used for le
 model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
 #Categorical_crossentropy used for loss function.
 
-epochs = 500  
+epochs = 500   
 batch_size = 100
-
 #data augmentation for increasing accuracy.
 datagen = ImageDataGenerator(
         featurewise_center=False,  # set input mean to 0 over the dataset
@@ -114,14 +115,28 @@ plt.ylabel("Loss")
 plt.legend()
 plt.show()
 
+plt.plot(history.history['loss'], color='b', label="train loss")
+plt.title("Train Loss")
+plt.xlabel("Number of Epochs")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+
+## 
+# 0 -> Benign
+# 1 -> Malignant
+# 2 -> Normal
+
+
 # confusion matrix
 import seaborn as sns
 # Predict the values from the validation dataset
-Y_pred = model.predict(X_val)
+Y_pred = model.predict(X_train)
 # Convert predictions classes to one hot vectors 
 Y_pred_classes = np.argmax(Y_pred,axis = 1) 
 # Convert validation observations to one hot vectors
-Y_true = np.argmax(Y_val,axis = 1) 
+Y_true = np.argmax(Y_train,axis = 1) 
 # compute the confusion matrix
 confusion_mtx = confusion_matrix(Y_true, Y_pred_classes) 
 # plot the confusion matrix
@@ -131,13 +146,23 @@ sns.heatmap(confusion_mtx, annot=True, linewidths=0.01,cmap="Greens",linecolor="
 
 plt.xlabel("Predicted Label")
 plt.ylabel("True Label")
-plt.title("Confusion Matrix")
+plt.title("Confusion Matrix of Training")
 plt.show()
 
 
+Y_pred2 = model.predict(X_val)
+# Convert predictions classes to one hot vectors 
+Y_pred_classes2 = np.argmax(Y_pred2,axis = 1) 
+# Convert validation observations to one hot vectors
+Y_true2 = np.argmax(Y_val,axis = 1) 
+# compute the confusion matrix
+confusion_mtx = confusion_matrix(Y_true2, Y_pred_classes2) 
+# plot the confusion matrix
+f,ax = plt.subplots(figsize=(8, 8))
 
+sns.heatmap(confusion_mtx, annot=True, linewidths=0.01,cmap="Greens",linecolor="gray", fmt= '.1f',ax=ax)
 
-
-
-
-
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+plt.title("Confusion Matrix of Validation")
+plt.show()
